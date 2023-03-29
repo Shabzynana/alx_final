ae4eda51d060881fff3ed679cfbcc6abbc57f212
Adebiyi Oluwafemi <horlarmihleykan10@gmail.com>
2023-03-29 12:38:30 +0100

    a

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -158,9 +158,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'horlarmihleykan10@gmail.com'
-EMAIL_HOST_PASSWORD = 'pyjermbnrlvrolxw'
+EMAIL_HOST = 'smtp.mail.yahoo.com'
+EMAIL_HOST_USER = 'horlarmihleykan10@yahoo.com'
+EMAIL_HOST_PASSWORD = 'Yewandee#17'
 EMAIL_PORT = 587
 
 PASSWORD_RESET_TIMEOUT = 300
